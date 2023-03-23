e647f8e32bbed17946de9b1ef6f962de1bea1e93
RudezY <rudyyamashiro@yahoo.com>
2023-03-22 18:12:22 -0700

    added h2h

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,6 +5,7 @@ import teamGen from '../assets/teamgen.jpg'
 import Gymder from '../assets/gymder_Login.jpg'
 import nosql from '../assets/nosql.jpg'
 import mysql from '../assets/mysql.jpg'
+import h2h from '../assets/h2h.png'
 import handlebars from '../assets/handlebars.jpg'
 
 
@@ -16,6 +17,7 @@ export default function Portfolio() {
               These are some of the projects and assignments that I have completed during my boot camp experience! These will be updated with personal side projects in the near future!
             </p>
             <div className='grid sm:grid-cols-2 gap-12'>
+                <a href="https://github.com/RudezY/heart-2-heart" target="_blank" rel="noopener noreferrer"><PortfolioItem img={h2h} title='Heart2Heart'/></a>
                 <a href="https://github.com/RudezY/GetYourDrinkON-" target="_blank" rel="noopener noreferrer"><PortfolioItem img={GYOD} title='GetYourDrinkOn'/></a>
                 <a href="https://github.com/RudezY/TeamProfileGen"target="_blank" rel="noopener noreferrer"><PortfolioItem img={teamGen} title='Team Profile Generator'/></a>
                 <a href="https://github.com/RudezY/Gymder"target="_blank" rel="noopener noreferrer"><PortfolioItem img={Gymder} title='Gymder'/></a>
--- src/components/PortfolioItems.jsx
+++ src/components/PortfolioItems.jsx
@@ -9,7 +9,7 @@ export default function PortfolioItem({img, title}) {
             <h3 className='text-2xl font-bold text-white tracking-wider text-center'>
                 {title}
             </h3>
-            <p className='pb-4 pt-2 text-white text-center'>JavaScript</p>
+            <p className='pb-4 pt-2 text-white text-center'></p>
             <a href="/">
             </a>
         </div>
